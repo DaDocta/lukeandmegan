da4d98e25af3e00026cc2eeb14a758306188a304
Beyond Understanding <127466825+b3yund@users.noreply.github.com>
2024-11-16 18:52:21 -0500

    Updated commit

---
+++ src/pages/LukeHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LukeHome = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to Luke's Home</h1>
+            <p>This is the homepage for Luke's side of the site.</p>
+        </div>
+    );
+};
+
+export default LukeHome;
---
+++ src/pages/MeganHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MeganHome = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to Megan's Home</h1>
+            <p>This is the homepage for Megan's side of the site.</p>
+        </div>
+    );
+};
+
+export default MeganHome;
