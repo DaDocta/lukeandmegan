991197c9fe9b65a407150490a4f88f9746258ebb
Beyond Understanding <127466825+b3yund@users.noreply.github.com>
2024-11-16 22:43:39 -0500

    Updated commit

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import About from './About';
 import Skills from './Skills';
 import Experience from './Experience';
@@ -6,11 +6,6 @@ import Contact from './Contact';
 import '../styles/Portfolio.css';
 
 const Portfolio = () => {
-  useEffect(() => {
-    const newUrl = "https://megancalme.site/portfolio";
-    window.history.pushState(null, '', newUrl);
-  }, []);
-
   return (
     <div className="portfolio-page">
       <About />
